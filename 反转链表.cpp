5be5cab4845d4fb5fe44cc13afe86136f0bdc976
Tangc <759249230@qq.com>
2018-04-16 16:43:41 +0800

    13

---
+++ 反转链表.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+//题目描述
+//输入一个链表，反转链表后，输出链表的所有元素。
+
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x)
+		:val(x)
+		, next(NULL)
+	{}
+};
+class Solution {
+public:
+	ListNode* ReverseList(ListNode* pHead) {
+		if (pHead == NULL)
+			return NULL;
+		ListNode* pre = NULL;
+		ListNode* next = NULL;
+		while (pHead)
+		{
+			next = pHead->next;
+			pHead->next = pre;
+			pre = pHead;
+			pHead = next;
+		}
+		return pre;
+	}
+};
