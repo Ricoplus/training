b50194e33079115ffd89aa986d5a8d29ecb0e1ac
Tangc <759249230@qq.com>
2018-04-22 13:52:21 +0800

    15

--- 顺时针打印矩阵.cpp
+++ 顺时针打印矩阵.cpp
@@ -6,32 +6,39 @@ using namespace std;
 //例如，如果输入如下矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
 //则依次打印出数字1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
 
+
+
+
 class Solution {
 public:
-	vector<int> printMatrix(vector<vector<int> > matrix) {
+	vector<int> printMatrix(vector<vector<int>> matrix) {
+
+		vector<int> v;
 		int row = matrix.size();
 		int col = matrix[0].size();
-		vector<int> res;
-
-		// 输入的二维数组非法，返回空的数组
-		if (row == 0 || col == 0)  return res;
-
-		// 定义四个关键变量，表示左上和右下的打印范围
-		int left = 0, top = 0, right = col - 1, bottom = row - 1;
-		while (left <= right && top <= bottom)
+		if (row == 0 || col == 0)
+			return v;
+		int left = 0,top = 0, right = col - 1,bottom = row - 1 ;
+		while (left<=right&&top<=bottom)
 		{
-			// left to right
-			for (int i = left; i <= right; ++i)  res.push_back(matrix[top][i]);
-			// top to bottom
-			for (int i = top + 1; i <= bottom; ++i)  res.push_back(matrix[i][right]);
-			// right to left
-			if (top != bottom)
-				for (int i = right - 1; i >= left; --i)  res.push_back(matrix[bottom][i]);
-			// bottom to top
-			if (left != right)
-				for (int i = bottom - 1; i > top; --i)  res.push_back(matrix[i][left]);
-			left++, top++, right--, bottom--;
+			//左边到右边
+			for (int i = left; i <= right; i++)
+				v.push_back(matrix[top][i]);
+			//上面到下边
+			for (int i = top + 1; i <=bottom; i++)
+				v.push_back(matrix[i][right]);
+			//右边到左边
+			if(top!=bottom)//防止单列
+			for (int i = right - 1; i >= left; i--)
+				v.push_back(matrix[bottom][i]);
+			//下边到上边
+
+			if(left!=right)//防止单行
+			for (int i = bottom-1; i> top; i--)
+				v.push_back(matrix[i][left]);
+			left++; top++; right--;  bottom--;
 		}
-		return res;
+		return v;
 	}
 };
+
