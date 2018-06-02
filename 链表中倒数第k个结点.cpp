c3b98cf1b46c7a3e6713c4e8fa21f9445ca43b23
Tangc <759249230@qq.com>
2018-06-02 21:03:10 +0800

    54

--- 链表中倒数第k个结点.cpp
+++ 链表中倒数第k个结点.cpp
@@ -17,35 +17,23 @@ struct ListNode {
 //制造了一个K长度的尺子, 把尺子从头往后移动, 当尺子的右端与链表的末尾对齐的时候，尺子左端所在的结点就是倒数第k个结点！
 class Solution {
 public:
-	ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
-		if (pListHead == NULL || k < 0)
+	ListNode * FindKthToTail(ListNode* pListHead, unsigned int k) {
+		if (k == 0 || pListHead == NULL)
 			return NULL;
-
-		ListNode* pre = pListHead;
-		ListNode* last = pListHead;
-		while (k--)
+		
+		ListNode* cur = pListHead;
+		//问题在于当是正着数第一个会有问题 直接会返回空 所以--k 不是k--
+		while (--k)
 		{
-			last = last->next;
-			if (last==NULL)
+			cur = cur->next;
+			if (cur == NULL)
 				return NULL;
 		}
-		while (last->next)
+		while (cur->next)
 		{
-			pre = pre->next;
-			last = last->next;
-		}
-		return pre;
-		/*if (pListHead == NULL)
-			return NULL;
-		vector<ListNode*> v;
-		ListNode* cur = pListHead;
-		while (cur)
-		{
-			v.push_back(cur);
+			pListHead = pListHead->next;
 			cur = cur->next;
 		}
-		if (v.size() - k >= v.size())
-			return NULL;
-		return v[v.size() - k];*/
+		return pListHead;
 	}
 };
